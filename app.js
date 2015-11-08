a9c004226f959cb76877aad33b5d898529e7f99f
EricSSartorius <EricSSartorius@gmail.com>
2015-11-08 17:19:39 +0900

    ui router up

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 if (Meteor.isClient) {
- 
-  angular.module('judging-system',['angular-meteor']);
- 
-  angular.module('judging-system').controller('JudgingSystemCtrl', ['$scope',
-    function ($scope) {
- 	$scope.title = "blah";
-//
- 
-  }]);
-}
+	angular.module('judging-system', ['angular-meteor', 'ui.router']); 
+	angular.module('judging-system').config(function($urlRouterProvider, $stateProvider, $locationProvider){
+
+      $locationProvider.html5Mode(true);
+
+      $stateProvider
+        .state('landing', {
+          url: '/landing',
+          templateUrl: 'landing.ng.html',
+          controller: 'LandingCtrl'
+        })
+        .state('judgingConsole', {
+          url: '/judging-console',
+          templateUrl: 'judging-console.ng.html',
+          controller: 'JudgingConsoleCtrl'
+        });
+
+      $urlRouterProvider.otherwise("/landing");
+    });
+  
+   angular.module('judging-system').controller('LandingCtrl', function ($scope, $meteor) {
+     $scope.title = "Landing";
+   });
+   angular.module('judging-system').controller('JudgingConsoleCtrl', function ($scope, $meteor) {
+     $scope.judge = "Judging Console";
+   });
+ }
