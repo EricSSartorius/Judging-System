9bc394fc679b292e158afdc95a89dfe0e52ead7a
EricSSartorius <EricSSartorius@gmail.com>
2015-11-09 01:55:48 +0900

    create-event page added

---
+++ client/controllers/create-event.ng.js
@@ -0,0 +1,3 @@
+angular.module('judging-system').controller('CreateEventCtrl', function ($scope, $meteor) {
+//
+});
--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,18 +1,24 @@
 angular.module('judging-system').config(function($urlRouterProvider, $stateProvider, $locationProvider){
-
-      $locationProvider.html5Mode(true);
-
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'client/views/home.ng.html',
-          controller: 'HomeCtrl'
-        })
-        .state('judgingConsole', {
-          url: '/judging-console',
-          templateUrl: 'client/views/judging-console.ng.html',
-          controller: 'JudgingConsoleCtrl'
-        });
-
-      $urlRouterProvider.otherwise("/home");
+  $locationProvider.html5Mode(true);
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'client/views/home.ng.html',
+      controller: 'HomeCtrl'
+    })
+    .state('judgingConsole', {
+      url: '/judging-console',
+      templateUrl: 'client/views/judging-console.ng.html',
+      controller: 'JudgingConsoleCtrl'
+    })
+    .state('createEvent', {
+      url: '/create-event',
+      templateUrl: 'client/views/create-event.ng.html',
+      controller: 'CreateEventCtrl'
+    })
+    .state('eventForm', {
+            url: "/eventForm",
+            template: UiRouter.template('eventForm.html')
     });
+  $urlRouterProvider.otherwise("/home");
+});
