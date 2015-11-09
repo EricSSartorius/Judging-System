7f1360af6eb19b80dcb95fe8ef486be7c51ac086
EricSSartorius <EricSSartorius@gmail.com>
2015-11-10 00:02:47 +0900

    logins finished

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,3 +1,11 @@
+angular.module("judging-system").run(function ($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('home');
+    }
+  });
+});
+
 angular.module('judging-system').config(function($urlRouterProvider, $stateProvider, $locationProvider){
   $locationProvider.html5Mode(true);
   $stateProvider
@@ -9,16 +17,22 @@ angular.module('judging-system').config(function($urlRouterProvider, $stateProvi
     .state('judgingConsole', {
       url: '/judging-console',
       templateUrl: 'client/views/judging-console.ng.html',
-      controller: 'JudgingConsoleCtrl'
+      controller: 'JudgingConsoleCtrl',
+      resolve: {
+        "currentUser": function($meteor){
+          return $meteor.requireUser();
+        }
+      }
     })
     .state('createEvent', {
       url: '/create-event',
       templateUrl: 'client/views/create-event.ng.html',
-      controller: 'CreateEventCtrl'
-    })
-    .state('eventForm', {
-            url: "/eventForm",
-            template: UiRouter.template('eventForm.html')
+      controller: 'CreateEventCtrl',
+      resolve: {
+        "currentUser": function($meteor){
+          return $meteor.requireUser();
+        }
+      }
     });
   $urlRouterProvider.otherwise("/home");
 });
