9caf16821aa313a6b72170659fcb87d8b25038d4
EricSSartorius <EricSSartorius@gmail.com>
2015-11-21 02:52:15 +0900

    style and admin console updates

--- client/controllers/admin-console.ng.js
+++ client/controllers/admin-console.ng.js
@@ -1,7 +1,8 @@
 angular.module('judging-system').controller('AdminConsoleCtrl', function ($scope, $meteor) {
-	$scope.player = "current player's name goes here";
-	$scope.round = "round goes here";
-	$scope.judge = "judges names go here"
-	$scope.score = "judge's submited score for the round goes here"
-	$scope.time = 'time left in game goes here';
+	$scope.player = "Player Name";
+	$scope.round = 1;
+	$scope.judge = "Judge Name";
+	$scope.score = 0;
+	$scope.time = '10:00';
+	$scope.total = 100;
 });
