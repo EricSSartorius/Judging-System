572b8ff2d7c403c989c1186704c0a5230243b6fc
EricSSartorius <EricSSartorius@gmail.com>
2015-11-20 18:51:15 +0900

    admin console updated

--- client/controllers/admin-console.ng.js
+++ client/controllers/admin-console.ng.js
@@ -1,3 +1,7 @@
 angular.module('judging-system').controller('AdminConsoleCtrl', function ($scope, $meteor) {
-
+	$scope.player = "current player's name goes here";
+	$scope.round = "round goes here";
+	$scope.judge = "judges names go here"
+	$scope.score = "judge's submited score for the round goes here"
+	$scope.time = 'time left in game goes here';
 });
