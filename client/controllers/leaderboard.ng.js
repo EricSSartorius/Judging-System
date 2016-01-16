157b367f219c8b3faa8dc248c83d3e3c56fba41f
EricSSartorius <EricSSartorius@gmail.com>
2016-01-16 15:54:17 -0500

    style pages organized

--- client/controllers/leaderboard.ng.js
+++ client/controllers/leaderboard.ng.js
@@ -1,6 +1,5 @@
 angular.module('judging-system').controller('LeaderboardCtrl', function ($scope, $meteor) {
 	$scope.event = Events.findOne({inGame:true});
-	$scope.totalScore = 0;
 	// if ($scope.event) {
 	// 	var playerScore = $scope.$meteorCollection(function(){
 	// 		$scope.event.find({"players.totalScore": $scope.totalScore});
