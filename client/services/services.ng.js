48d70aae8701849e0067ac4985f4195ba1423ac4
EricSSartorius <EricSSartorius@gmail.com>
2016-01-20 23:59:37 -0500

    sidebar working

--- client/services/services.ng.js
+++ client/services/services.ng.js
@@ -44,25 +44,3 @@ angular.module('judging-system').factory('TimeFactory', function($interval) {
 //         }
 //     };
 // });
-
-
-
-
-
-// angular.module('judging-system').directive('alertDirective', function() {
-
-//      return {
-//          template: '<div>'+'<i  ng-click="alertAdmin()" ng-show="alert" class="fa fa-dot-circle-o">'+'</i>'+'<i  ng-click="alertAdmin()" ng-hide="alert" class="fa fa-circle-o">'+'</i>'+'</div>',
-//          restrict: 'E',
-//          replace: true,
-//          scope: {},
-//          link: function(scope, element, attributes) {
-//         	scope.alert = false;
-        	
-//         	scope.alertAdmin = function() {
-// 	        	scope.alert = (!scope.alert) ? true : false;
-// 	        	return scope.alert;
-// 	        }
-//     	}
-//     }
-//  });
