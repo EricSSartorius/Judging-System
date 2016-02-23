b44140624c05b10f64952e7dfb0d7e237e2d325d
Felice DeNigris <felideni@gmail.com>
2016-02-22 22:49:56 -0800

    Highlights the tied players with a color red

--- client/controllers/leaderboard.ng.js
+++ client/controllers/leaderboard.ng.js
@@ -1,10 +1,46 @@
 angular.module('judging-system').controller('LeaderboardCtrl', function ($scope, $meteor) {
 	$scope.event = Events.findOne({inGame:true});
+
+	$scope.eventplayers = Events.findOne({inGame:true}).players;
+
+	var contestants = $scope.eventplayers;
+
+	$scope.tied = contestants.map(function(player){
+		var output = {}
+		output.name = player.name
+		output.score = player.totalScore
+		return output
+	}).sort(function(a,b){
+		return parseFloat(a.score) - parseFloat(b.score)
+	}).reverse()
+
+	var checkforTie = $scope.tied.slice(); //[{"name":"iron man","score":7},{"name":"cap america","score":3},{"name":"hulk","score":7}]
+
+	var weHaveATie = function(array){
+	  var start = 0
+	  var tiedplayers = []
+	  //loop through array to see equal scores
+	  for(var i = 0; i < array.length; i++){
+	    if(array[start].score === array[i].score){
+	      //if scores are equal to checkforTie[0] push to tiedplayers
+	      tiedplayers.push(array[i])
+	    }
+	  }
+	  if(tiedplayers.length > 1){
+	    console.log("weHaveATie, says 'There is a tie!'", tiedplayers)
+	    return tiedplayers
+	  }
+	  else{
+	    console.log("weHaveATie, says 'False, There's no tie'")
+	    return false
+	  }
+	};
+
+	$scope.dowehaveTie = weHaveATie(checkforTie);
+
 	window.scope=$scope;
 	if ($scope.event) {
-		$scope.rank = "Rank";
-		$scope.name = "Name";
-		$scope.points = "Points";
+		$scope.title = "Rank   Name   Points";
 
 		$scope.events = $scope.$meteorCollection(function(){
 			return Events.find({inGame:true});
@@ -14,4 +50,3 @@ angular.module('judging-system').controller('LeaderboardCtrl', function ($scope,
 		$scope.noEvent = "No event currently running";
 	}
 });
- 
