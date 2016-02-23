0c59ef390a1ee256c05184d3d1ce860bc20b920e
Felice DeNigris <felideni@gmail.com>
2016-02-23 11:14:46 -0800

    Tie players functionality fully working with javascript and data binding

--- client/controllers/leaderboard.ng.js
+++ client/controllers/leaderboard.ng.js
@@ -1,6 +1,8 @@
 angular.module('judging-system').controller('LeaderboardCtrl', function ($scope, $meteor) {
 	$scope.event = Events.findOne({inGame:true});
 
+
+	if($scope.event !== undefined){
 	$scope.eventplayers = Events.findOne({inGame:true}).players;
 
 	var contestants = $scope.eventplayers;
@@ -17,17 +19,17 @@ angular.module('judging-system').controller('LeaderboardCtrl', function ($scope,
 	var checkforTie = $scope.tied.slice(); //[{"name":"iron man","score":7},{"name":"cap america","score":3},{"name":"hulk","score":7}]
 
 	var weHaveATie = function(array){
-	  var start = 0
-	  var tiedplayers = []
+	  var start = 0;
+	  var tiedplayers = [];
 	  //loop through array to see equal scores
 	  for(var i = 0; i < array.length; i++){
 	    if(array[start].score === array[i].score){
 	      //if scores are equal to checkforTie[0] push to tiedplayers
-	      tiedplayers.push(array[i])
+	      tiedplayers.push(array[i]);
 	    }
 	  }
 	  if(tiedplayers.length > 1){
-	    console.log("weHaveATie, says 'There is a tie!'", tiedplayers)
+	    console.log("weHaveATie, says 'There is a tie!'", tiedplayers);
 	    return tiedplayers
 	  }
 	  else{
@@ -37,6 +39,10 @@ angular.module('judging-system').controller('LeaderboardCtrl', function ($scope,
 	};
 
 	$scope.dowehaveTie = weHaveATie(checkforTie);
+}//end if $scope.event !== undefined
+
+	//check to see if there is an event game
+	$scope.nogame = Events.findOne({inGame:true}) === undefined ? "No Game" : "Game on";
 
 	window.scope=$scope;
 	if ($scope.event) {
